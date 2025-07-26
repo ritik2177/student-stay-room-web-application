24276c74d5f440e1948faf23e8b28af8e7a2086f
Ritik Kumar <ritikkatsa2005@gmail.com>
2025-07-27 02:10:23 +0530

    Update navbar.tsx

--- src/app/dashboard/navbar.tsx
+++ src/app/dashboard/navbar.tsx
@@ -108,3 +108,73 @@ export default function Navbar() {
     </nav>
   );
 }
+
+
+
+
+
+//this is for home page
+'use client'
+
+export default function Home() {
+
+
+  return (
+    <div>
+    <div className="relative bg-[url('/image/image1.png')] bg-cover bg-center h-screen text-white">
+
+  {/* Overlay */}
+  <div className=""></div>
+
+  {/* Content */}
+  <div className="flex flex-col justify-center h-full px-6 md:px-16">
+    
+    {/* Text Section */}
+    <div className="mb-3 ml-4">
+      <h1 className="text-5xl  max-w-96 font-bold mb-1 drop-shadow-lg text-gray-500">
+        Welcome to Student Stey
+      </h1>
+      <p className="text-lg leading-relaxed  max-w-[500px] text-gray-400 drop-shadow-sm">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo magni
+        necessitatibus beatae quia facilis eaque dolor.
+      </p>
+    </div>
+
+    {/* Search Section */}
+    <div className="w-full max-w-4xl bg-white/70 backdrop-blur-md rounded-xl p-4 shadow-lg">
+  <div className="flex flex-col md:flex-row gap-4 w-full">
+    
+    <input
+      type="text"
+      placeholder="Enter your center name"
+      className="w-full p-3 rounded-md border border-gray-300 placeholder-gray-500 text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    />
+    
+    <input
+      type="date"
+      className="w-full p-3 rounded-md border border-gray-300 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    />
+    
+    <input
+      type="number"
+      placeholder="Enter area PIN Code"
+      className="w-full p-3 rounded-md border border-gray-300 placeholder-gray-500 text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+    />
+    
+    <button className="w-full md:w-auto bg-blue-700 hover:bg-blue-800 text-white px-6 py-3 rounded-md transition">
+      Search
+    </button>
+
+  </div>
+</div>
+
+
+  </div>
+</div>
+
+   
+    </div>
+    
+  );
+}
+
