b1d8a12a34c048ace138ac22bef64fbd01712251
ritik2177 <ritikkatsa2005@gmail.com>
2025-08-01 09:00:03 -0400

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Navbar from '@/components/navbar'
 
 const page = () => {
+
+
+
   return (
-    <div>page</div>
+    <div>
+      pageeeeeeeeeee
+    </div>
   )
 }
 
